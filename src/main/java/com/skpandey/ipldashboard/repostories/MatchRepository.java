47995611ca7fd97ccb362cc410289f9e508e4f4b
Sandeep Pandey <skpandey86@gmail.com>
2021-05-09 16:10:56 +0100

    Get matchs by team name and year API completed

--- src/main/java/com/skpandey/ipldashboard/repostories/MatchRepository.java
+++ src/main/java/com/skpandey/ipldashboard/repostories/MatchRepository.java
@@ -1,15 +1,33 @@
 package com.skpandey.ipldashboard.repostories;
 
 import com.skpandey.ipldashboard.model.Match;
+import org.apache.tomcat.jni.Local;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
 
     List<Match> findByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
+
+    @Query("select m from Match m where (m.team1 = :teamName or m.team2 = :teamName) and m.date between :startDate and :endDate order by m.date desc")
+    List<Match> findMatchByTeamBetweenDates(
+            @Param("teamName") String teamName,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+        );
+    /*
+    List<Match> findByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(
+            String teamName1, LocalDate date1, LocalDate date2,
+            String teamName2, LocalDate date3, LocalDate date4
+        );
+    */
+
     default List<Match> findLatestMatchesByTeam(String teamName, int count) {
         return findByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0,count));
     }
--- src/main/java/com/skpandey/ipldashboard/repostories/TeamRepository.java
+++ src/main/java/com/skpandey/ipldashboard/repostories/TeamRepository.java
@@ -3,7 +3,6 @@ package com.skpandey.ipldashboard.repostories;
 import com.skpandey.ipldashboard.model.Team;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
 
 public interface TeamRepository extends CrudRepository<Team, Long> {
 
