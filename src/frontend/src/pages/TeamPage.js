eef8cd79fd87e0940dc88897e0bd2958247f8697
Sandeep Pandey <skpandey86@gmail.com>
2021-05-12 17:07:40 +0100

    Commit from Visual Studio Code

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
-import './TeamPage.css';
+import './TeamPage.scss';
 
 
 
@@ -45,7 +45,7 @@ export const TeamPage = () => {
       </div>
       {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match} />)}
       <div className="more-link">
-        <a href="#">More '&gt'</a>
+        <a href="#">More >></a>
       </div>
     </div>
   );
