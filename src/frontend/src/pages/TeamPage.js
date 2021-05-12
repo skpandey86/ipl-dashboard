757ce0a54c0d424b76c2a5c247fb3667820aacf1
Sandeep Pandey <skpandey86@gmail.com>
2021-05-12 05:55:34 +0100

    Add match page in react app

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -3,6 +3,9 @@ import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
+import './TeamPage.css';
+
+
 
 
 export const TeamPage = () => {
@@ -25,10 +28,25 @@ export const TeamPage = () => {
   }
   return (
     <div className="TeamPage">
-      <h1>{team.teamName}</h1>
-      <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
-      {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match} />)}
+      <div className="team-name-section">
+        <h1 class="team-name">{team.teamName}</h1>
+      </div>
+      <div className="win-loss-section">
+        Wins / Losses
+        <br/>
+        Wins = {team.totalWins}
+        <br/>
+        Wins = {team.totalMatches - team.totalWins}
 
+      </div>
+      <div className="match-detail-section">
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+      </div>
+      {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match} />)}
+      <div className="more-link">
+        <a href="#">More '&gt'</a>
+      </div>
     </div>
   );
 }
